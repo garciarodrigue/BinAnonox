5a8eedad3ca02b8c87c1bd0406b4b6beff89364f
TENSHI101 <48865498+garciarodrigue@users.noreply.github.com>
2022-03-05 01:32:07 -0600

    Bomba
    Falta edicion

---
+++ Ejecutor.sh
@@ -0,0 +1,56 @@
+PS1='\033[93mH4CK3R\033[1;94m[\033[1;96m\W\033[1;94m]\033[1;92m:\033[1;91m# \033[1;92m'
+
+echo -e '\e[38;5;93m'
+cat << 'EOF'
+                                                 ,--,  ,.-.
+                     ,                   \       '-,-`,'-.' | ._
+                    /|         \    ,    |\         }  )/  / `-,',
+                   | ;         |\  /|    | |       /  \|  |/`  ,`
+                   | |       ,.`  `,` `, | |  _,...(   (      .',
+                   \  \  __ ,-` `  ,  , `/ |,'      Y     (   /_L\
+                    \  \_\,``,   ` , ,  /  |         )         _,/
+                     \  '  `  ,_ _`_,-,<._.<        /         /
+                      ', `>.,`  `  `   ,., |_      |         /
+                        \/`  `,   `   ,`  | /__,.-`    _,   `\
+                    -,-..\  _  \  `  /  ,  / `._) _,-\`       \
+                     \_,,.) /\    ` /  / ) (-,, ``    ,        |
+                    ,` )  | \_\       '-`  |  `(               \
+                   /  /```(   , --, ,' \   |`<`    ,            |
+                  /  /_,--`\   <\  V /> ,` )<_/)  | \      _____)
+           ,-, ,`   `   (_,\ \    |   /) / __/  /   `----`
+           (-, \           ) \ ('_.-._)/ /,`    /
+           | /  `          `/ \\ V   V, /`     /
+        ,--\(        ,     <_/`\\     ||      /
+       (   ,``-     \/|         \-A.A-`|     /
+      ,>,_ )_,..(    )\          -,,_-`  _--`
+     (_ \|`   _,/_  /  \_            ,--`
+      \( `   <.,../`     `-.._   _,-`
+
+EOF
+echo -e '\e[0m'
+
+sleep 1.0
+
+setterm -foreground red
+echo "BIENVENDIDO A BINARIOS ANONOX"
+sleep 2.0
+
+setterm -foreground green
+echo "Creado x TENSHI"     
+sleep 2.0
+
+setterm -foreground blue                                                       echo "Doble sifrado de contraseñas" 
+sleep 2.0
+
+setterm -foreground green 
+echo "La Contraseña es: skullbinanonox101"
+
+sleep 3.0
+
+cd Bomba
+
+gpg skull.sh.gpg
+
+sleep 1.0
+
+bash skull.sh
---
+++ install.sh
@@ -0,0 +1,12 @@
+echo "instalando requerimientos"
+sleep 1.0
+
+pkg install figlet -y
+
+pip install gnupg 
+
+echo "exitoso"
+
+sleep 2.0
+
+echo "ya puedes proceder"
